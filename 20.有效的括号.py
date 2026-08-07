7c1f0e38aa08749d98e9e8989a76d1e4f4296efa
Ryan Zhu <2715744657@qq.com>
2026-08-07 23:23:28 +0800

    完成 20.有效的括号

---
+++ 20.有效的括号.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=20 lang=python3
+#
+# [20] 有效的括号
+#
+
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        i = 0
+        stack = []
+        while i < len(s):
+            if s[i] in ['(', '[', '{']:
+                stack.append(s[i])
+            else:
+                if not stack:
+                    return False
+                if s[i] == ')' and stack[-1] != '(':
+                    return False
+                if s[i] == ']' and stack[-1] != '[':
+                    return False
+                if s[i] == '}' and stack[-1] != '{':
+                    return False
+                stack.pop()
+            i += 1
+        return not stack
+# @lc code=end
+
