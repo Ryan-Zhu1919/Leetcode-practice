e306f8e334aae04ba7610cd01fa10606615b4fd1
Ryan Zhu <2715744657@qq.com>
2026-08-09 21:21:52 +0800

    完成 69.x的平方根

---
+++ 69.x-的平方根.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=69 lang=python3
+#
+# [69] x 的平方根 
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
+# @lc code=end
+
