e27171e8382d72258ebec339185e2c84bb3c0fe4
Ryan Zhu <2715744657@qq.com>
2026-08-10 20:41:01 +0800

    完成 860.柠檬水找零

---
+++ 860.柠檬水找零.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=860 lang=python3
+#
+# [860] 柠檬水找零
+#
+
+# @lc code=start
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+        for c in bills:
+            if c == 5:
+                five += 1
+            elif c == 10:
+                five -= 1
+                ten += 1
+            elif ten > 0:
+                ten -= 1
+                five -= 1
+            else:
+                five -= 3
+            if five < 0:
+                return False
+        return True
+# @lc code=end
+
