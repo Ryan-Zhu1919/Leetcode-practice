79a5e60ec41b99fe4c0dea29c1259f50db955059
Ryan Zhu <2715744657@qq.com>
2026-08-19 23:32:13 +0800

    完成 27.移除元素

---
+++ 27.移除元素.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode.cn id=27 lang=python3
+#
+# [27] 移除元素
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
+# @lc code=end
+
