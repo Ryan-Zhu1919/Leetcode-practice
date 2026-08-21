808689ec8af6ae150d9fc53e8a2e06094f55f331
Ryan Zhu <2715744657@qq.com>
2026-08-21 23:11:34 +0800

    完成 92.反转链表

---
+++ 92.反转链表-ii.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode.cn id=92 lang=python3
+#
+# [92] 反转链表 II
+#
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy = ListNode(next = head)
+        p0 = dummy
+        for _ in range(left - 1):
+            p0 = p0.next
+        pre = None
+        cur = p0.next
+        for _ in range(right - left + 1):
+            nxt = cur.next
+            cur.next = pre
+            pre = cur
+            cur =nxt
+        p0.next.next = cur
+        p0.next = pre
+        return dummy.next
+# @lc code=end
+
