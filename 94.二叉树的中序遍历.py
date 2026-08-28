860f3f7423542b1ab08cc30c300e895dd7c4d0a0
Ryan Zhu <2715744657@qq.com>
2026-08-28 23:24:21 +0800

    完成 94.二叉树的中序遍历

---
+++ 94.二叉树的中序遍历.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=94 lang=python3
+#
+# [94] 二叉树的中序遍历
+#
+from typing import Optional, List
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        res = list(res)
+        return res
+# @lc code=end
+
