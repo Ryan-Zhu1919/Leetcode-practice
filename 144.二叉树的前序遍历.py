463c4ecff7054a1576a70078a5b23fe8da8498f2
Ryan Zhu <2715744657@qq.com>
2026-08-28 23:20:40 +0800

    完成 144.二叉树的前序遍历

---
+++ 144.二叉树的前序遍历.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=144 lang=python3
+#
+# [144] 二叉树的前序遍历
+#
+from typing import Optional, List
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            if not root:
+                return
+            res.append(root.val)
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        res = list(res)
+        return res
+# @lc code=end
+
