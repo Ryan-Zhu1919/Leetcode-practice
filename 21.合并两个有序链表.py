f619fa640e6e7f3fd9d8eeb3dfd4265bc4da527a
Ryan Zhu <2715744657@qq.com>
2026-08-30 23:21:32 +0800

    完成 21.合并两个有序链表

--- 21.合并两个有序链表.py
+++ 21.合并两个有序链表.py
@@ -3,29 +3,41 @@
 #
 # [21] 合并两个有序链表
 #
-
+from typing import Optional
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(0)
-        cur = head
-        while list1 and list2:
-            if list1.val < list2.val:
-                cur.next = list1
-                list1 = list1.next
-            else:
-                cur.next = list2
-                list2 = list2.next
-            cur = cur.next
-        if list1:
-            cur.next = list1
-        if list2:
-            cur.next = list2
-        return head.next
+        # head = ListNode(0)
+        # cur = head
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         cur.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         cur.next = list2
+        #         list2 = list2.next
+        #     cur = cur.next
+        # if list1:
+        #     cur.next = list1
+        # if list2:
+        #     cur.next = list2
+        # return head.next
+
+        #递归法
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list2.next, list1)
+            return list2 
 # @lc code=end
 
