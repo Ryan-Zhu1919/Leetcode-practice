5e9a99f94c2dbcf97f8e8a99964377bc344e5e5a
Ryan Zhu <2715744657@qq.com>
2026-08-30 20:47:41 +0800

    完成 1.两数之和

--- 1.两数之和.py
+++ 1.两数之和.py
@@ -3,13 +3,20 @@
 #
 # [1] 两数之和
 #
-
+from typing import List
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # 暴力
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        hashtable = dict()
+        for i, num in enumerate(nums):
+            if target - num in hashtable:
+                return [hashtable[target - num], i]
+            hashtable[num] = i
+        return []
 # @lc code=end
 
