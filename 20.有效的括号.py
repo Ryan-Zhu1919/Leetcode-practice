e2977582b758ac5fb6fe7a28ce26c9f67687b84b
Ryan Zhu <2715744657@qq.com>
2026-08-30 20:58:08 +0800

    完成 20.有效的括号

--- 20.有效的括号.py
+++ 20.有效的括号.py
@@ -7,10 +7,27 @@
 # @lc code=start
 class Solution:
     def isValid(self, s: str) -> bool:
+        # i = 0
+        # stack = []
+        # while i < len(s):
+        #     if s[i] in ['(', '[', '{']:
+        #         stack.append(s[i])
+        #     else:
+        #         if not stack:
+        #             return False
+        #         if s[i] == ')' and stack[-1] != '(':
+        #             return False
+        #         if s[i] == ']' and stack[-1] != '[':
+        #             return False
+        #         if s[i] == '}' and stack[-1] != '{':
+        #             return False
+        #         stack.pop()
+        #     i += 1
+        # return not stack
         i = 0
-        stack = []
+        stack, left = [], ['(', '[', '{']
         while i < len(s):
-            if s[i] in ['(', '[', '{']:
+            if s[i] in left:
                 stack.append(s[i])
             else:
                 if not stack:
@@ -21,8 +38,9 @@ class Solution:
                     return False
                 if s[i] == '}' and stack[-1] != '{':
                     return False
-                stack.pop()
+                stack.pop() 
             i += 1
-        return not stack
+        return not stack        
+
 # @lc code=end
 
