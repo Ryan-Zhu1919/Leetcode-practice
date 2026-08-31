017e1907ef4ae847adaf304775432932c8e358dd
Ryan Zhu <2715744657@qq.com>
2026-08-31 20:34:14 +0800

    完成 169.多数元素

--- 169.多数元素.py
+++ 169.多数元素.py
@@ -4,15 +4,27 @@
 # [169] 多数元素
 #
 from typing import List
-from collections import Counter
+# from collections import Counter
 # @lc code=start
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        cnt = Counter()
-        n = len(nums)
-        for c in nums:
-            cnt[c] += 1
-            if cnt[c] > n // 2:
-                return c
+        # 暴力
+        # cnt = Counter()
+        # n = len(nums)
+        # for c in nums:
+        #     cnt[c] += 1
+        #     if cnt[c] > n // 2:
+        #         return c
+        
+        # 摩尔投票法
+        ans, votes = 0, 0
+        for num in nums:
+            if votes == 0:
+                ans = num
+            if num == ans:
+                votes += 1
+            else:
+                votes -= 1
+        return ans
 # @lc code=end
 
