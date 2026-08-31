07b6d11999d3944c31138fb7a1871dfd04ca7499
Ryan Zhu <2715744657@qq.com>
2026-08-31 20:22:44 +0800

    完成 141.环形链表

--- 141.环形链表.py
+++ 141.环形链表.py
@@ -4,17 +4,12 @@
 # [141] 环形链表
 #
 from typing import Optional
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fast = slow = head
