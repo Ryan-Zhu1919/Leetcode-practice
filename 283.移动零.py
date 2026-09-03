e8c601080f49ae36ad315abb195ad482ee9d2b4a
Ryan Zhu <2715744657@qq.com>
2026-09-03 20:34:41 +0800

    完成 283.移动零

--- 283.移动零.py
+++ 283.移动零.py
@@ -3,18 +3,28 @@
 #
 # [283] 移动零
 #
-
+from typing import List
 # @lc code=start
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        left, right = 0, 0
-        while right < len(nums):
-            if nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
-                left += 1
-            right += 1
+        # 双指针
+        # left, right = 0, 0
+        # while right < len(nums):
+        #     if nums[right] != 0:
+        #         nums[left], nums[right] = nums[right], nums[left]
+        #         left += 1
+        #     right += 1
+
+        # 栈
+        size = 0
+        for num in nums:
+            if num:
+                nums[size] = num
+                size += 1
+        for i in range(size, len(nums)):
+            nums[i] = 0
                 # @lc code=end
 
