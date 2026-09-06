ecb05f8bfdb65ace7d484cc29d790320928f84ae
Ryan Zhu <2715744657@qq.com>
2026-09-06 22:00:45 +0800

    完成 392.判断子序列

---
+++ 392.判断子序列.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=392 lang=python3
+#
+# [392] 判断子序列
+#
+
+# @lc code=start
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        if not s: return True
+        i = 0
+        for c in t:
+            if s[i] == c:
+                i += 1
+                if i == len(s):
+                    return True
+        return False
+# @lc code=end
+
