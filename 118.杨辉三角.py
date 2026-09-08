ae7a02b620c4a0c57005f849977868651083165d
Ryan Zhu <2715744657@qq.com>
2026-09-08 23:31:24 +0800

    完成 118.杨辉三角

---
+++ 118.杨辉三角.py
@@ -0,0 +1,16 @@
+#
+# @lc app=leetcode.cn id=118 lang=python3
+#
+# [118] 杨辉三角
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        c = [[1] * (i + 1) for i in range(numRows)]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                c[i][j] = c[i - 1][j - 1] + c[i - 1][j]
+        return c
+# @lc code=end
+
