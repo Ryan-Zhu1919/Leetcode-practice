15c26ede2d6eb4de557f81cf3aa27561fbc4d90f
Ryan Zhu <2715744657@qq.com>
2026-09-08 23:28:02 +0800

    完成 414.第三大的数

---
+++ 414.第三大的数.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode.cn id=414 lang=python3
+#
+# [414] 第三大的数
+#
+from typing import List
+# @lc code=start
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        diff = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                diff += 1
+                if diff == 3: 
+                    return nums[i]
+        return nums[0]
+# @lc code=end
+
