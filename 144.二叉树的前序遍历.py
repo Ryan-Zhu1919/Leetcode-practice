910030dcd27f7857ac32966608d319d8f0bbb726
Ryan Zhu <2715744657@qq.com>
2026-09-09 22:37:19 +0800

    完成 144.二叉树的前序遍历

--- 144.二叉树的前序遍历.py
+++ 144.二叉树的前序遍历.py
@@ -13,15 +13,34 @@ class TreeNode:
         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # 递归法
+        # res = []
+        # def dfs(root):
+        #     if not root:
+        #         return
+        #     res.append(root.val)
+        #     dfs(root.left)
+        #     dfs(root.right)
+        # dfs(root)
+        # return res
+
+        # 迭代法
         res = []
-        def dfs(root):
-            if not root:
-                return
-            res.append(root.val)
-            dfs(root.left)
-            dfs(root.right)
-        dfs(root)
-        res = list(res)
+        stack = []
+        if root:
+            stack.append(root)
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+                stack.append(node)
+                stack.append(None)
+            else:
+                node = stack.pop()
+                res.append(node.val)
         return res
 # @lc code=end
 
