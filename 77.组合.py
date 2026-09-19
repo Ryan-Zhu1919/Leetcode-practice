7850ef5d3d33355557e8bffce5ae7c561eb0119d
Ryan Zhu <2715744657@qq.com>
2026-09-19 20:46:22 +0800

    完成 77.组合

---
+++ 77.组合.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=77 lang=python3
+#
+# [77] 组合
+#
+
+# @lc code=start
+class Solution:
+    def combine(self, n: int, k: int) -> list[list[int]]:
+        res = []
+        self.backtracking(n, k, 1, [], res)
+        return res
+    def backtracking(self, n, k, startindex, path, res):
+            if len(path) == k:
+                res.append(path[:])
+                return
+            for i in range(startindex, n + 1):
+                path.append(i)
+                self.backtracking(n, k, i + 1, path, res)
+                path.pop()
+# @lc code=end
+
